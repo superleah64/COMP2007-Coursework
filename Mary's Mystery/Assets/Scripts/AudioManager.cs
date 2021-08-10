8b3109fb4c98132a400f8190c7aac00531817170
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2021-08-10 03:51:24 +0100

    Pause menu done, game over screen created, more objects added
    Pause menu complete, temporary game over screen created, more trees and bushes added, more items added

--- Mary's Mystery/Assets/Scripts/AudioManager.cs
+++ Mary's Mystery/Assets/Scripts/AudioManager.cs
@@ -1,19 +1,25 @@
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play (string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
 }
--- Mary's Mystery/Assets/Scripts/MainMenu.cs
+++ Mary's Mystery/Assets/Scripts/MainMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // opens scene 1, the game, when method is run
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // quits the application when method is run
     public void QuitGame()
     {
         Application.Quit();

b74ad7606c627c4f381dc95bb60d31cfcc6b942a
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2021-08-17 13:36:16 +0100

    Win screen working
    Player wins the game when all the items are collected

--- Mary's Mystery/Assets/Scripts/GameController.cs
+++ Mary's Mystery/Assets/Scripts/GameController.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int collectable = 0;
-    public bool collectedAll = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,21 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (collectable <= 0)
+        {
+            YouWin();
+        }
+        // plays sound effect and destroys object
+        void YouWin()
+        {
+            StartCoroutine(WinScreen());
+        }
+
+        // prevents win screen from being displayed until pickup is complete
+        IEnumerator WinScreen()
+        {
+            yield return new WaitForSeconds(3f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
     }
 }
